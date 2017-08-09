2251b2be4bf707597f2df2af8676d8d9f0961ac3
Jeff <Home@Jeffs-MacBook-Pro.local>
2017-08-09 23:54:27 +0200

    added onclick on all td elements

--- app.js
+++ app.js
@@ -2,13 +2,26 @@ class Game {
     constructor(p1, p2) {
         this.player1 = p1;
         this.player2 = p2;
+        this.winner = "";
         this.grid = [["", "", ""],["", "", ""], ["", "", ""]];
     }
 
-    play() {
-            var square = document.getElementById("zero-zero");
-            console.log(square.Id);
+    play(identity) {
+        let mark = this.player1;
+        let idList = ["#zero-zero", "#zero-one", "#zero-two", "#one-zero", "#one-one", "#one-two", "#two-zero", "#two-one", "#two-two"];
+            let square = document.querySelector("#zero-zero");
+            square.innerHTML = mark; 
     }
+
+
+    quare(m, lst) {
+    }
+
+
+
+
+
+
 
 
 
@@ -33,8 +46,7 @@ let player2 = new Player("O");
 let player3 = "0";
 console.log(typeof player3);
 console.log(player1.getMark());
-let game = new Game(player1, player2);
-game.play();
+let game = new Game(player1.getMark(), player2.getMark());
 
 
 
--- tic-tac-toe.js
+++
@@ -1,7 +0,0 @@
-class Tic-tac-toe {
-    constructor(r, c) {
-        this.row = r;
-        this.column = c;
-        this.grid = [["", "", ""], ["", "", ""], ["", "", ""]];
-    }
-}
